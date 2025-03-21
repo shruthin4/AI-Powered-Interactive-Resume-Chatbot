d16bbb4a4a66bdd692147fbbe2f6c2b2e4719074
Shruthin Reddy Sainapuram <115421303+shruthin4@users.noreply.github.com>
2025-03-21 18:54:44 -0500

    Update Backend_flask.py

--- Backend_flask.py
+++ Backend_flask.py
@@ -295,7 +295,7 @@ def cleanup_html(text):
     return text
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
 
